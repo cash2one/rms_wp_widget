40b8fe0b99c81d2c1cba8ad5e1c5528887eccc45
vincent.leung.cn <vincent.leung.cn@gmail.com>
2014-09-24 11:56:54 +0800

    add date range

--- egg_rms_wp_widget/egg_rms_wp_widget.php
+++ egg_rms_wp_widget/egg_rms_wp_widget.php
@@ -13,6 +13,7 @@ Version: 0.1
 define( 'EGGRMS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 //add setting menu
 require_once (EGGRMS__PLUGIN_DIR."egg_rms_wp_widget_setting.php");
+require_once (EGGRMS__PLUGIN_DIR."egg_rms_wp_widget_footer.php");
 
 
 
--- egg_rms_wp_widget/egg_rms_wp_widget_footer.php
+++ egg_rms_wp_widget/egg_rms_wp_widget_footer.php
@@ -6,7 +6,11 @@ $pt=strip_tags($post->post_content);
 $pt=urlencode(substr($pt,0,900));
 $wp_egg_rms_number = get_option("EGG_NUMBER_OF_RMS");
 $wp_egg_rms_date_range = get_option("EGG_DATE_RANGE_OF_RMS");
-$url="http://106.185.30.33:3000/showwidget?index=theegg&type=article&domain=cosmopolitan.co.hk&type&content=".$pt."&maxc=".$wp_egg_rms_number."&range=".$wp_egg_rms_date_range;
+
+$url="http://106.185.30.33:3000/showwidget?index=theegg&type=article&domain=cosmopolitan.com.hk&type&content=".$pt."&maxc=".$wp_egg_rms_number;
+if($wp_egg_rms_date_range>1){
+   $url=$url."&range=".$wp_egg_rms_date_range;
+}
 $iframe="<iframe style='width:600px;height:200px;border:none;' src=".$url."></iframe>";
 
 		return $content."Maybe You Like These Articles:".$iframe;
